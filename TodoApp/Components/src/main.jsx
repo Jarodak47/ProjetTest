fffe894f978291bc31ccab800650ae2af799fb33
Severin Fernando Tanga Mvondo <jarodak47@gmail.com>
2025-05-19 17:08:00 +0100

    Refactoring:removing AppTest.jsx file

--- TodoApp/Components/src/main.jsx
+++ TodoApp/Components/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './AppTest.jsx' //importation du composant principal à modifier si vous souhaitez tester localement 
+import App from './App.jsx' //importation du composant principal à modifier si vous souhaitez tester localement 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
